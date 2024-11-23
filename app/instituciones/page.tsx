6456014be97ee8004701ed6cf4a78046ea2af52f
cristiancutizaca <cutizacamachacacristian@gmail.com>
2024-11-23 18:02:20 -0500

    Progress was made with the institutions section, errors were solved in responsive, changes and problems in dark and litgh modes were changed and solved.

--- app/instituciones/page.tsx
+++ app/instituciones/page.tsx
@@ -19,49 +19,72 @@ const Graduate = () => {
         "Descubre ECOMÁS, tu destino para diplomados y cursos especializados que impulsan tu desarrollo profesional. Explora más de 70 diplomados y únete a nuestra comunidad de aprendizaje práctico y avanzado. ¡Empieza hoy!",
       date: "February 15, 2024",
       category: "ECOMAS",
-      image: "/image/ecomas.webp",
+      image: "/image/ecomas2.webp",
+      link: "https://ecomas.pe/", // Agregar enlace aquí si disponible
     },
     {
       title:
         "RIZO proporciona cursos y diplomados especializados en ingeniería y más, con educación flexible a tu medida y precios accesibles. ¡Capacítate con expertos y avanza en tu carrera!",
       date: "February 15, 2024",
       category: "RIZO",
-      image: "/image/rizo.webp",
+      image: "/image/rizo2.webp",
+      link: "https://rizo.edu.pe/",
     },
     {
       title:
-        "CIMADE ofrece diplomados y certificaciones para profesionales en áreas como manejo ambiental y seguridad alimentaria. Su página web clara y organizada facilita el acceso a cursos y contactos, posicionando a CIMADE como un socio esencial en el desarrollo profesional.",
+        "CIMADE ofrece cursos y diplomados en manejo ambiental y seguridad, diseñados para tu crecimiento profesional. Con precios accesibles y flexibilidad educativa, ¡capacítate en CIMADE y avanza en tu carrera!",
       date: "February 15, 2024",
       category: "CIMADE",
       image: "/image/cimade.webp",
+      link: "https://cimade.edu.pe/",
     },
     {
       title:
-        "A significant shift in mindset is required to support other type of side project.",
+        "PROMAS ofrece una amplia gama de diplomados y cursos en ingeniería, gestión ambiental y más, diseñados para adaptarse a tus necesidades profesionales. Con métodos de enseñanza flexibles y costos competitivos, PROMAS te prepara con expertos del sector para que logres tus objetivos de carrera. ¡Capacítate en PROMAS y toma el control de tu desarrollo profesional!",
       date: "February 15, 2024",
-      category: "Security",
-      image: "/image/4.jpg",
+      category: "PROMAS",
+      image: "/image/promas.webp",
+      link: "https://www.promas.edu.pe/",
     },
     {
       title:
-        "Population change anything what your need for your next generation.",
+        "BINEX ofrece cursos y diplomados en ingeniería, arqueología vial y más, adaptados a las necesidades del mercado actual. Con opciones de estudio en línea, BINEX permite a los profesionales capacitarse desde cualquier lugar, promoviendo una educación flexible y accesible. ¡Avanza en tu carrera con BINEX!",
       date: "February 15, 2024",
-      category: "Security",
-      image: "/image/5.jpg",
+      category: "binex",
+      image: "/image/binex.webp",
+      link: "https://binex.edu.pe/",
     },
     {
       title:
-        "How to improve your business so that you can stay in your local business.",
+        "Explora y certifícate con SAYAN, tu aliado en el camino hacia el éxito profesional. Ofrecemos una amplia variedad de cursos y diplomados en áreas críticas como manejo de residuos sólidos, calidad ambiental y gestión de proyectos. Con SAYAN, alcanza nuevos límites en tu carrera a través de un aprendizaje innovador y práctico, disponible completamente en línea. ¡Únete a más de 7,000 estudiantes que han transformado su futuro profesional con nosotros!",
       date: "February 15, 2024",
-      category: "Security",
-      image: "/image/6.jpg",
+      category: "SAYAN",
+      image: "/image/sayan.webp",
+      link: "https://sayan.edu.pe/",
+    },
+
+    {
+      title:
+        "¡Transforma tu futuro con SEVEDA! Descubre cursos y diplomados que abren puertas a nuevas oportunidades en un mundo que evoluciona rápidamente. ¡Inscríbete hoy y construye un mundo mejor!",
+      date: "February 15, 2024",
+      category: "SEVEDA",
+      image: "/image/seveda.webp",
+      link: "https://seveda.edu.pe/",
+    },
+    {
+      title:
+        "¡Impulsa tu futuro con INALTA! Descubre cursos y diplomados que abren caminos hacia oportunidades ilimitadas en campos críticos como ingeniería, gestión ambiental y más. ¡Únete a INALTA y lidera la transformación del mañana!",
+      date: "February 15, 2024",
+      category: "INALTA",
+      image: "/image/inalta.jpg",
+      link: "https://inalta.edu.pe/",
     },
   ];
 
   return (
     <section id="/cursos">
       {/* Header */}
-      <div className="relative pt-20 pb-10 bg-[url('/image/inst-fond.jpg')] bg-cover bg-center bg-no-repeat bg-black opacity-50">
+      <div className="relative pt-20 pb-10 bg-[url('/image/inst-fond.jpg')] bg-cover bg-center bg-no-repeat  dark:bg-black dark:opacity-70 ">
         <div className="relative mx-auto inset-0 flex flex-col justify-center items-center text-center text-gray-800 px-4 mt-40">
           <h1 className="font-extrabold text-4xl lg:text-6xl mt-10 text-black">
             INSTITUCIONES AFILIADAS
@@ -73,12 +96,12 @@ const Graduate = () => {
       </div>
 
       {/* Blog Section */}
-      <div className="p-10 bg-[#16213e]">
+      <div className="p-10 bg-[#e0f9f6] dark:bg-fondDark ">
         <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto">
           {blogPosts.map((post, index) => (
             <div
               key={index}
-              className="bg-[#1e2a47] rounded-lg shadow-md overflow-hidden flex flex-col"
+              className="bg-[#009FB2] dark:bg-[#1e2a47] rounded-lg shadow-md overflow-hidden flex flex-col"
             >
               {/* Fixed height for images */}
               <div className="h-80">
@@ -97,12 +120,25 @@ const Graduate = () => {
                     {post.title}
                   </h2>
                 </div>
-                <a
-                  href="#"
-                  className="text-blue-500 mt-4 inline-block hover:underline"
-                >
-                  View Details &gt;
-                </a>
+                {post.link ? (
+                  <a
+                    href={post.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mt-4"
+                  >
+                    <button className="bg-black text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition md:ml-[120px] ls:ml-2" >
+                      Ver página
+                    </button>
+                  </a>
+                ) : (
+                  <button
+                    disabled
+                    className="bg-gray-500 text-white py-2 px-4 rounded-lg cursor-not-allowed mt-4"
+                  >
+                    No disponible
+                  </button>
+                )}
               </div>
             </div>
           ))}
--- components/home/PrincipalHome.jsx
+++ components/home/PrincipalHome.jsx
@@ -1,12 +1,11 @@
-// PrincipalHome.js
 "use client";
+
 import React from "react";
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import "./stylesHome.css";
 import { TypeAnimation } from "react-type-animation";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { useTheme } from "next-themes";
 import HeroImage from "./hero-image"; // Asegúrate de que la ruta sea correcta
 
@@ -18,20 +17,19 @@ const PrincipalHome = () => {
   };
 
   return (
-    // Ajuste de padding-top para dejar espacio debajo del Navbar
-    <section className="relative flex justify-center items-center pt-20 md:pt-32 lg:pt-40 pl-8 bg-fixed bg-gradient-to-r  dark:from-fondDark dark:to-fondDark">
+    <section className="relative flex justify-center items-center pt-20 md:pt-32 lg:pt-40 pl-8 bg-fixed bg-gradient-to-r dark:from-fondDark dark:to-fondDark">
       <div className="absolute w-96 h-96 blur-2xl -z-10 bg-gradient-radial dark:from-cyan-950 from-blue-200 to-transparent right-[11%] top-0 hidden md:block"></div>
-      <div className="absolute w-96 h-96 blur-2xl -z-10 bg-gradient-radial dark:from-sky-950 from-violet-200 right-96 top-10 "></div>
+      <div className="absolute w-96 h-96 blur-2xl -z-10 bg-gradient-radial dark:from-sky-950 from-violet-200 right-96 top-10"></div>
       <div className="absolute w-96 h-96 blur-2xl -z-10 bg-gradient-radial dark:from-cyan-950 from-blue-100 to-transparent left-[10%] top-[10%] hidden md:block"></div>
-      
+
       {/* Botón para cambiar de tema */}
       <button
         className="fixed z-50 top-16 right-0 mr-3 p-2 flex items-center justify-center"
         onClick={handleThemeChange}
-      >
-      </button>
+      ></button>
 
       <div className="py-8 lg:py-10 max-w-7xl sm:px-6 lg:px-8 flex flex-col md:flex-row items-start w-full">
+        {/* Texto principal */}
         <motion.div
           variants={{
             hidden: { opacity: 0, y: 100, x: "-100%" },
@@ -61,17 +59,17 @@ const PrincipalHome = () => {
               wrapper="span"
               cursor={true}
               repeat={Infinity}
-              className="text-gray-700 dark:text-white text-5xl md:text-5xl lg:text-6xl font-semibold"
+              className="text-gray-700 dark:text-white text-4xl md:text-5xl lg:text-6xl font-semibold"
             />
           </h1>
-          <h1 className="text-[#009FB2] text-7xl md:text-6xl lg:text-7xl font-extrabold mb-2">
+          <h1 className="text-[#009FB2] text-5xl md:text-6xl lg:text-7xl font-extrabold mb-2">
             VERICERTS
           </h1>
-          <p className="mb-8 max-w-lg font-normal text-2xl md:text-xl lg:text-2xl text-gray-50">
+          <p className="mb-8 max-w-lg font-normal text-lg md:text-xl lg:text-2xl text-gray-50">
             {/* Contenido adicional puede ir aquí */}
           </p>
         </motion.div>
-        
+
         {/* HeroImage para pantallas grandes */}
         <motion.div
           className="hidden md:block md:mr-5 mt-8 md:mt-0 moveArrow"

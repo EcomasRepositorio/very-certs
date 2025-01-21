74d1a575eccddce13912b13a7ea1141b3718cc87
zunniga <jmoficial2003@gmail.com>
2025-01-21 15:37:01 -0500

    add the component of but corporation

--- components/home/ComoVeri.jsx
+++ components/home/ComoVeri.jsx
@@ -1,64 +1,72 @@
 import React from "react";
 import ScrollAnimation from "./scrollAnimation";
+import {
+  Settings,
+  FileText,
+  Database,
+  Globe,
+  CheckCircle,
+  BarChart2,
+} from "lucide-react"; // Importamos íconos necesarios
 
 const NuestrosDiplomados = () => {
   return (
     <ScrollAnimation>
-      <div className="flex flex-col md:flex-row items-center bg-transparent p-8 md:p-20 rounded-lg max-w-screen-xl mx-auto">
+      <div className="flex flex-col md:flex-row items-center bg-transparent p-6 sm:p-8 md:p-20 rounded-lg max-w-screen-xl mx-auto space-y-8 md:space-y-0">
         {/* Contenedor de texto */}
-        <div className="md:w-1/2 p-4 md:pr-8">
+        <div className="md:w-1/2 p-4 md:pr-8 text-center md:text-left">
           <span className="bg-customBlue text-white py-1 px-4 rounded-full text-sm inline-block mb-4">
             NOSOTROS
           </span>
-          <div className="text-3xl md:text-4xl font-extrabold text-gray-700 dark:text-white leading-tight mb-4">
-            ¿ Cómo Funciona ?
-          </div>
+          <h2 className="text-3xl sm:text-4xl font-extrabold text-gray-700 dark:text-white leading-tight mb-6">
+            ¿Cómo Funciona?
+          </h2>
           <p className="text-gray-700 dark:text-gray-300 font-poppins mb-6 leading-relaxed">
             Verycerts conecta a instituciones y verificadores en un sistema
             seguro y eficiente, simplificando la emisión y verificación de
             certificados educativos.
           </p>
-          <p className="text-gray-600 text-lg font-poppins font-bold dark:text-white mb-6">
+          <h3 className="text-gray-600 text-lg font-poppins font-bold dark:text-white mb-4">
             Para Instituciones Educativas
-          </p>
+          </h3>
           <ul className="space-y-3 mb-6">
             <li className="flex items-center">
-              <span className="text-blue-500 text-xl mr-2">✔</span>
+              <Settings className="text-customBlue text-xl mr-2" />
               <span className="font-poppins">Registro y Configuración</span>
             </li>
             <li className="flex items-center">
-              <span className="text-blue-500 text-xl mr-2">✔</span>
+              <FileText className="text-customBlue  text-xl mr-2" />
               <span className="font-poppins">Emisión</span>
             </li>
             <li className="flex items-center">
-              <span className="text-blue-500 text-xl mr-2">✔</span>
+              <Database className="text-customBlue text-xl mr-2" />
               <span className="font-poppins">Gestión y Almacenamiento</span>
             </li>
           </ul>
-          <p className="text-gray-600 font-poppins font-bold dark:text-white mb-6">
-            Para Verificadores{" "}
-          </p>
-          <ul className="space-y-3 mb-6">
+          <h3 className="text-gray-600 font-poppins font-bold dark:text-white mb-4">
+            Para Verificadores
+          </h3>
+          <ul className="space-y-3">
             <li className="flex items-center">
-              <span className="text-blue-500 text-xl mr-2">✔</span>
+              <Globe className="text-customBlue  text-xl mr-2" />
               <span className="font-poppins">Acceso al Portal</span>
             </li>
             <li className="flex items-center">
-              <span className="text-blue-500 text-xl mr-2">✔</span>
+              <CheckCircle className="text-customBlue  text-xl mr-2" />
               <span className="font-poppins">Verificación en Tiempo Real</span>
             </li>
             <li className="flex items-center">
-              <span className="text-blue-500 text-xl mr-2">✔</span>
+              <BarChart2 className="text-customBlue text-xl mr-2" />
               <span className="font-poppins">Informes</span>
             </li>
           </ul>
         </div>
 
-        {/* Contenedor de imagen con botón de reproducción */}
+        {/* Contenedor de video */}
         <div className="md:w-1/2 flex justify-center items-center mt-8 md:mt-0 relative">
-          <div className="relative lg:w-[500px] lg:max-w-md rounded-lg overflow-hidden shadow-lg md:lg:w-[300px]">
+          <div className="relative lg:w-[500px] lg:max-w-md rounded-none overflow-hidden  md:lg:w-[300px]">
             <img
-              src="/image/vericert1.webp" // Reemplaza esta ruta con la imagen que desees
+              src="/certificate/image/test.webp" // Reemplaza esta ruta con la imagen que desees
               alt="Tech Solution Image "
               className="w-[500px] h-[500px] object-cover "
             />
